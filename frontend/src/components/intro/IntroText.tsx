65f5c1cc823a6527a84e564f059a73d1cb58ea49
Nahuel <carlos.89.nsc@gmail.com>
2026-01-29 02:08:42 -0300

    glitch title

--- frontend/src/components/intro/IntroText.tsx
+++ frontend/src/components/intro/IntroText.tsx
@@ -7,7 +7,7 @@ type IntroTextProps = {
 const IntroText = ({ onEnd }: IntroTextProps) => {
   return (
     <div className="intro-text">
-      <h1>KYNKI</h1>
+      <h1 className="kynki" data-text="KYNKI">KYNKI</h1>
 
       <button data-text="Enter" onClick={onEnd}>
         Enter
